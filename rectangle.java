17e51cd7ca0d99fc55c64e66386d3edd1e3ff2e7
anshika <reachanshika29@gmail.com>
2022-08-19 18:25:18 +0530

    Area of rectangle

---
+++ rectangle.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class rectangle {
+
+        public static void main(String []args) {
+            Scanner sc = new Scanner(System.in);
+            System.out.println("Enter the base and height: ");
+            double b = sc.nextDouble();
+            double h = sc.nextDouble();
+            double a = b*h;
+            System.out.println("Area of rectangle:"+ a);   
+        }
+        
+    }
+    
+
