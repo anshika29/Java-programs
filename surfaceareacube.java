b9eb3ac28e7e885564cf5ae765c3877eb5a62574
anshika <reachanshika29@gmail.com>
2022-08-19 20:32:45 +0530

    Surface area of cube

---
+++ surfaceareacube.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class surfaceareacube {
+    public static void main(String [] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the side : ");
+        double a = sc.nextDouble();
+        double s = 6 * a * a;
+        System.out.println("Surface area of cube : " + s);
+    }
+}
