f5fa961fb2887d85969c8304caedc45fd13ff828
anshika <reachanshika29@gmail.com>
2022-08-19 19:13:13 +0530

    Perimeter of equilateral triangle

---
+++ perimeterequitriangle.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class perimeterequitriangle {
+    public static void main(String[]args) {
+        try (Scanner sc = new Scanner(System.in)) {
+            System.out.println("Enter the side");
+            double r = sc.nextDouble();
+            double p = 3 * r;
+            System.out.println("Area of circle : " + p);
+        }   
+    }
+}
