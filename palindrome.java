9f2d8f95b105817be5c50e407895bbb401cea0e8
anshika <reachanshika29@gmail.com>
2022-08-20 20:14:10 +0530

    palindrome number

---
+++ palindrome.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class palindrome {
+   public static void main(String []args){
+    Scanner sc = new Scanner(System.in);
+    System.out.println("Enter a number: ");
+    int n = sc.nextInt();
+    int t=n,rev=0;
+    while(t>0){
+        int r = t%10;
+        rev = rev*10+r;
+        t=t/10;
+    }
+    if (rev==n)
+    System.out.println(n+ " is a palindrome number.");
+    else
+    System.out.println(n+ " is not a palindrome number");
+   } 
+}
